245c5b93eb487cf579a48beb0537de6c9a94b4ff
xuxiaoqing <xuxiaoqing@1582597470426922.onaliyun.com>
2020-07-15 08:54:02 +0800

    修改：redis cache

--- spring-boot-demo-cache-redis/src/main/java/com/juejueguai/cache/redis/SpringBootDemoCacheRedisApplication.java
+++ spring-boot-demo-cache-redis/src/main/java/com/juejueguai/cache/redis/SpringBootDemoCacheRedisApplication.java
@@ -2,8 +2,10 @@ package com.juejueguai.cache.redis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class })
 public class SpringBootDemoCacheRedisApplication {
 
     public static void main(String[] args) {
